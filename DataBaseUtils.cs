9b57531362b046e122abea2005140b9b953d1628
Александр Пертенава <zwelenewskiy@yandex.ru>
2019-11-15 20:13:28 +0300

    Изменен метод отправки писем

--- DataBaseUtils.cs
+++ DataBaseUtils.cs
@@ -8,34 +8,61 @@ namespace DV_server
 {
     public class DataBaseUtils
     {
-        private static readonly string PATH = @"C:\Users\Pertenava.A\Основное задание\Сервер\DV_server\bin\Debug\DBconnection.ini";
+        private static readonly string PATH = @"C:\Users\Pertenava.A\Основное задание\Сервер\DV_server\bin\DBconnection.ini";
 
         /// <summary>
         /// Возращает строку подключения к БД
         /// </summary>
         public static string ReadConnectSettings(string path)
         {
-            string result = @"Data Source=.\<source>;Initial Catalog=<catalog>;Integrated Security=True";
+            //string result = @"Data Source=.\<source>;Initial Catalog=<catalog>;Integrated Security=True";
+            string result = @"Data Source=<source>;Initial Catalog=<catalog>;Password=<password>;User ID=<login>;Integrated Security=True";
 
             result = result.Replace("<source>", INIfileUtils.ReadKey(path, "mssql", "server"));
             result = result.Replace("<catalog>", INIfileUtils.ReadKey(path, "mssql", "catalog"));
+
+           if(INIfileUtils.ReadKey(path, "mssql", "need_auth") == "1")
+           {
+                result = result.Replace("<password>", INIfileUtils.ReadKey(path, "mssql", "password"));
+                result = result.Replace("<login>", INIfileUtils.ReadKey(path, "mssql", "login"));
+            }
 
             return result;
         }
 
         public static List<Email> GetRecords()
         {
-            /*using (SqlConnection connection = new SqlConnection(ReadConnectSettings(PATH)))
+            List<Email> result = new List<Email>();
+
+            using (SqlConnection connection = new SqlConnection(ReadConnectSettings(PATH)))
             {
                 connection.Open();
 
-                Console.WriteLine(connection.State == System.Data.ConnectionState.Open);
+                var t = new SqlCommand("SELECT id, name, date, content, ФИО FROM DV_Test_server").ExecuteReader();
+
+                using(SqlDataReader reader = new SqlCommand("SELECT * FROM emails").ExecuteReader())
+                {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            Email tmp_email = new Email()
+                            {
+                                id = Convert.ToInt32(reader["id"]),
+                                name = reader["id"].ToString(),
+                                date = reader["id"].ToString(),
+                                content = reader["id"].ToString(),
+                                FIO = reader["id"].ToString(),
+                            };
+                        }                        
+                    }
+                }
 
                 connection.Close();
-            }*/
+            }
 
-            Console.WriteLine(ReadConnectSettings(PATH));
-            return new List<Email>();
+
+            return result;
         }
     }
 }
--- Email.cs
+++ Email.cs
@@ -9,5 +9,10 @@ namespace DV_server
     [DataContract]
     public class Email
     {
+        public int id;
+        public string name;
+        public string date;
+        public string content;
+        public string FIO;
     }
 }
