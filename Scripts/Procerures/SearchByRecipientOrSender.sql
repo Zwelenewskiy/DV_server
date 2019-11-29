a1491d008352a18cec4d3ffb90bc1ea04e6e0da7
Александр Пертенава <zwelenewskiy@yandex.ru>
2019-11-29 19:45:57 +0300

    ...

--- Scripts/Procerures/SearchByRecipientOrSender.sql
+++ Scripts/Procerures/SearchByRecipientOrSender.sql
@@ -1,107 +1,58 @@
-ALTER PROCEDURE SearchByRecipientOrSender
+CREATE PROCEDURE SearchByRecipientOrSender
 	@recipients xml = NULL,--получатели [to]
 	@senders xml = NULL--отправители [from]
 AS
 BEGIN
-
-	IF ((@recipients IS NOT NULL) AND (@senders IS NOT NULL))
+	WITH recepients(id, email, name)
+	AS
+	(
 		SELECT 
-			em.[id], 
-			em.[from], 
-			em.[date], 
-			em.[content], 
-			em.[name]
+			t.n.value('(id)[1]', 'int') AS 'id',
+			t.n.value('(email)[1]', 'nvarchar(20)') AS 'email',
+			t.n.value('(name)[1]', 'nvarchar(20)') AS 'name'
 		FROM
-			email em
-		WHERE em.[from] IN 
-		(
-			SELECT
-				n.id
-			FROM
-			(
-				SELECT 
-					t.n.value('(id)[1]', 'int') AS 'id',
-					t.n.value('(email)[1]', 'nvarchar(20)') AS 'email',
-					t.n.value('(name)[1]', 'nvarchar(20)') AS 'name'
-				FROM
-					@senders.nodes('/ArrayOfUser/User') t(n) 
-			) n
-			JOIN [user] ON n.email = [user].email--сделать на user.email индекс
-		)
-		
-		UNION
+			@recipients.nodes('/ArrayOfUser/User') t(n) 
+	),
+	senders(id, email, name)
+	AS
+	(
+		SELECT 
+			t.n.value('(id)[1]', 'int') AS 'id',
+			t.n.value('(email)[1]', 'nvarchar(20)') AS 'email',
+			t.n.value('(name)[1]', 'nvarchar(20)') AS 'name'
+		FROM
+			@senders.nodes('/ArrayOfUser/User') t(n) 
+	)
 
-		SELECT
-			em.[id], 
-			em.[from], 
-			em.[date], 
-			em.[content], 
-			em.[name]
-		FROM
-			email em
-		JOIN [to] t ON em.id = t.email_id
-		WHERE t.[user_id] IN
-		(
-			SELECT
-				n.id
-			FROM
-			(
-				SELECT 
-					t.n.value('(id)[1]', 'int') AS 'id',
-					t.n.value('(email)[1]', 'nvarchar(20)') AS 'email',
-					t.n.value('(name)[1]', 'nvarchar(20)') AS 'name'
-				FROM
-					@recipients.nodes('/ArrayOfUser/User') t(n) 
-			) n
-			JOIN [user] ON n.email = [user].email--сделать на user.email индекс
-		)	
-		ORDER BY em.id
-	ELSE IF(@recipients IS NOT NULL)
-		SELECT
-			em.[id], 
-			em.[from], 
-			em.[date], 
-			em.[content], 
-			em.[name]
-		FROM
-			email em
-		JOIN [to] t ON em.id = t.email_id
-		WHERE t.[user_id] IN
-		(
-			SELECT
-				n.id
-			FROM
-			(
-				SELECT 
-					t.n.value('(id)[1]', 'int') AS 'id',
-					t.n.value('(email)[1]', 'nvarchar(20)') AS 'email',
-					t.n.value('(name)[1]', 'nvarchar(20)') AS 'name'
-				FROM
-					@recipients.nodes('/ArrayOfUser/User') t(n) 
-			) n
-			JOIN [user] ON n.email = [user].email--сделать на user.email индекс
-		)
-	ELSE IF(@senders IS NOT NULL)--COALESCE(...) - возвращает первое не null-значение: поиск по имени ИЛИ email
-		SELECT  
-			*
-		FROM
-			email em
-		WHERE em.[from] IN
-		(
-			SELECT
-				n.id
-			FROM
-			(
-				SELECT 
-					t.n.value('(id)[1]', 'int') AS 'id',
-					t.n.value('(email)[1]', 'nvarchar(20)') AS 'email',
-					t.n.value('(name)[1]', 'nvarchar(20)') AS 'name'
-				FROM
-					@senders.nodes('/ArrayOfUser/User') t(n) 
-			) n
-			JOIN [user] ON n.email = [user].email--сделать на user.email индекс
-		)
+	SELECT 
+		e.[id], 
+		e.[from], 
+		e.[date], 
+		e.[content], 
+		e.[name]
+	FROM
+		email e
+	JOIN [to] t ON e.id = t.email_id
+	JOIN recepients r ON t.[user_id] = r.id
+	JOIN [user] u ON r.email = u.email OR r.name = u.name
+	
+	UNION
+
+	SELECT
+		e.[id], 
+		e.[from], 
+		e.[date], 
+		e.[content], 
+		e.[name]
+	FROM
+		email e	
+	JOIN senders s ON e.[from] = s.id
+	JOIN [user] u ON s.email = u.email OR s.name = u.name
 END;
 
-exec SearchByRecipientOrSender NULL, --'<ArrayOfUser><User><id>0</id><name>Иван</name><patronymic>Иванович</patronymic><lastname>Иванов</lastname><email>iva@tt.yy</email></User></ArrayOfUser>',
-							   '<ArrayOfUser><User><id>5</id><name>Иван</name><patronymic>Иванович</patronymic><lastname>Иванов</lastname><email>ivPetr@dfg.tt</email></User></ArrayOfUser>'
+exec SearchByRecipientOrSender NULL,--'<ArrayOfUser><User><id>0</id><name>Иван</name><patronymic>Иванович</patronymic><lastname>Иванов</lastname><email>iva@tt.yy</email></User></ArrayOfUser>',
+							   '<ArrayOfUser><User><id>5</id><name>Иван</name><patronymic>Сидорович</patronymic><lastname>Петров</lastname><email></email></User>ivPetr@dfg.tt</ArrayOfUser>'--
+
+select * from email
+select * from [user]
+select * from [to]
